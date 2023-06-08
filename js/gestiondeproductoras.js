be57b6858c167f00fc9e99aa1181aeb02976087a
elielberrio <eliel.berrio@cecar.edu.co>
2023-06-07 23:49:42 -0500

    más

--- js/gestiondeproductoras.js
+++ js/gestiondeproductoras.js
@@ -1,29 +1,29 @@
 function agregarALaTablaTelefono() {
-    var tablaTelefono = document.getElementById("tablaTelefono");
+  var tablaTelefono = document.getElementById("tablaTelefono");
 
-    var telefono = document.getElementById("telefono").value;
+  var telefono = document.getElementById("telefono").value;
 
-    var fila = tablaTelefono.insertRow();
+  var fila = tablaTelefono.insertRow();
 
-    var columnaTelefonos = fila.insertCell(0);
+  var columnaTelefonos = fila.insertCell(0);
 
-    columnaTelefonos.innerHTML = telefono;
+  columnaTelefonos.innerHTML = telefono;
 }
 
 function agregarALaTablaTelefonoCelular() {
-    var tablaTelefonoCelular = document.getElementById("tablaTelefonoCelular");
+  var tablaTelefonoCelular = document.getElementById("tablaTelefonoCelular");
 
-    var telefonoCelular = document.getElementById("telefonoCelular").value;
+  var telefonoCelular = document.getElementById("telefonoCelular").value;
 
-    var fila = tablaTelefonoCelular.insertRow();
+  var fila = tablaTelefonoCelular.insertRow();
 
-    var columnaTelefonos = fila.insertCell(0);
+  var columnaTelefonos = fila.insertCell(0);
 
-    columnaTelefonos.innerHTML = telefonoCelular;
+  columnaTelefonos.innerHTML = telefonoCelular;
 }
 
 function eliminarDeLaTablaTelefono() {
-    // Obtener la referencia a la tabla
+  // Obtener la referencia a la tabla
   var tabla = document.getElementById("tablaTelefono");
 
   // Obtener todas las filas de la tabla
@@ -34,14 +34,14 @@ function eliminarDeLaTablaTelefono() {
     // Obtener la celda de la primera columna de la fila
     var celda = filas[i].getElementsByTagName("td")[0];
 
-      tabla.deleteRow(i);
+    tabla.deleteRow(i);
 
-      break;
-}
+    break;
   }
+}
 
-  function eliminarDeLaTablaTelefonoCelular() {
-    // Obtener la referencia a la tabla
+function eliminarDeLaTablaTelefonoCelular() {
+  // Obtener la referencia a la tabla
   var tabla = document.getElementById("tablaTelefonoCelular");
 
   // Obtener todas las filas de la tabla
@@ -52,70 +52,70 @@ function eliminarDeLaTablaTelefono() {
     // Obtener la celda de la primera columna de la fila
     var celda = filas[i].getElementsByTagName("td")[0];
 
-      tabla.deleteRow(i);
+    tabla.deleteRow(i);
 
-      break;
-}
+    break;
   }
+}
 
-  function agregarALaTabla() {
+function agregarALaTabla() {
 
-    var tablaProductoras = document.getElementById("tablaProductoras");
+  var tablaProductoras = document.getElementById("tablaProductoras");
 
-    var codigo = document.getElementById("codigo").value;
-    var nombre = document.getElementById("nombre").value;
-    var pais = document.getElementById("pais").value;
-    var representante = document.getElementById("representante").value;
-    var telefono = document.getElementById("tablaTelefono").value + ", ";
+  var codigo = document.getElementById("codigo").value;
+  var nombre = document.getElementById("nombre").value;
+  var pais = document.getElementById("pais").value;
+  var representante = document.getElementById("representante").value;
+  var telefono = document.getElementById("tablaTelefono").value + ", ";
 
-    var columnaTelefono = [];
-    
-    // Iterar sobre las filas de la tabla (a partir de la segunda fila)
-    for (var i = 1; i < tablaTelefono.rows.length; i++) {
-      var fila = tablaTelefono.rows[i];
-      var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
-      columnaTelefono.push(celdaTelefono);
-    }
-    // También puedes utilizar los valores para mostrarlos en otro lugar o realizar otras operaciones
-    var telefono = columnaTelefono;
+  var columnaTelefono = [];
 
-    var correo = document.getElementById("correo").value;
-    var telefonoCelular = document.getElementById("tablaTelefonoCelular").value + ", ";
-    
-    var columnatelefonoCelular = [];
-    
-    // Iterar sobre las filas de la tabla (a partir de la segunda fila)
-    for (var i = 1; i < tablaTelefonoCelular.rows.length; i++) {
-      var fila = tablaTelefonoCelular.rows[i];
-      var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
-      columnatelefonoCelular.push(celdaTelefono);
-    }
-    // También puedes utilizar los valores para mostrarlos en otro lugar o realizar otras operaciones
-    var telefonoCelular = columnatelefonoCelular;
+  // Iterar sobre las filas de la tabla (a partir de la segunda fila)
+  for (var i = 1; i < tablaTelefono.rows.length; i++) {
+    var fila = tablaTelefono.rows[i];
+    var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
+    columnaTelefono.push(celdaTelefono);
+  }
+  // También puedes utilizar los valores para mostrarlos en otro lugar o realizar otras operaciones
+  var telefono = columnaTelefono;
 
-    var fila = tablaProductoras.insertRow();
+  var correo = document.getElementById("correo").value;
+  var telefonoCelular = document.getElementById("tablaTelefonoCelular").value + ", ";
 
-    var columnaCodigo = fila.insertCell(0);
-    var columnaNombre = fila.insertCell(1);
-    var columnaPais= fila.insertCell(2);
-    var columnaRepresentante = fila.insertCell(3);
-    var columnatelefono = fila.insertCell(4);
-    var columnacorreo = fila.insertCell(5);
-    var columnatelefonoCelular = fila.insertCell(6);
+  var columnatelefonoCelular = [];
+
+  // Iterar sobre las filas de la tabla (a partir de la segunda fila)
+  for (var i = 1; i < tablaTelefonoCelular.rows.length; i++) {
+    var fila = tablaTelefonoCelular.rows[i];
+    var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
+    columnatelefonoCelular.push(celdaTelefono);
+  }
+  // También puedes utilizar los valores para mostrarlos en otro lugar o realizar otras operaciones
+  var telefonoCelular = columnatelefonoCelular;
+
+  var fila = tablaProductoras.insertRow();
+
+  var columnaCodigo = fila.insertCell(0);
+  var columnaNombre = fila.insertCell(1);
+  var columnaPais = fila.insertCell(2);
+  var columnaRepresentante = fila.insertCell(3);
+  var columnatelefono = fila.insertCell(4);
+  var columnacorreo = fila.insertCell(5);
+  var columnatelefonoCelular = fila.insertCell(6);
 
 
-    columnaCodigo.innerHTML = codigo;
-    columnaNombre.innerHTML = nombre;
-    columnaPais.innerHTML = pais;
-    columnaRepresentante.innerHTML = representante;
-    columnatelefono.innerHTML = telefono;
-    columnacorreo.innerHTML = correo;
-    columnatelefonoCelular.innerHTML = telefonoCelular;
-   
+  columnaCodigo.innerHTML = codigo;
+  columnaNombre.innerHTML = nombre;
+  columnaPais.innerHTML = pais;
+  columnaRepresentante.innerHTML = representante;
+  columnatelefono.innerHTML = telefono;
+  columnacorreo.innerHTML = correo;
+  columnatelefonoCelular.innerHTML = telefonoCelular;
+
 }
 
 function eliminarDeLaTabla() {
-    // Obtener la referencia a la tabla
+  // Obtener la referencia a la tabla
   var tabla = document.getElementById("tablaProductoras");
 
   // Obtener todas las filas de la tabla
@@ -126,56 +126,56 @@ function eliminarDeLaTabla() {
     // Obtener la celda de la primera columna de la fila
     var celda = filas[i].getElementsByTagName("td")[0];
 
-      tabla.deleteRow(i);
+    tabla.deleteRow(i);
 
-      break;
+    break;
+  }
 }
+
+function guardarProductora() {
+
+  var productoras = JSON.parse(localStorage.getItem("productoras") || "[]");;
+
+  var codigo = document.getElementById("codigo").value;
+  var nombre = document.getElementById("nombre").value;
+  var pais = document.getElementById("pais").value;
+  var representante = document.getElementById("representante").value;
+  var telefono = document.getElementById("tablaTelefono").value + ", ";
+
+  var columnaTelefono = [];
+
+  // Iterar sobre las filas de la tabla (a partir de la segunda fila)
+  for (var i = 1; i < tablaTelefono.rows.length; i++) {
+    var fila = tablaTelefono.rows[i];
+    var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
+    columnaTelefono.push(celdaTelefono);
   }
 
-  function guardarProductora() {
+  var telefono = columnaTelefono;
 
-    var productoras = JSON.parse(localStorage.getItem("productoras") || "[]");;
+  var correo = document.getElementById("correo").value;
+  var telefonoCelular = document.getElementById("tablaTelefonoCelular").value + ", ";
 
-    var codigo = document.getElementById("codigo").value;
-    var nombre = document.getElementById("nombre").value;
-    var pais = document.getElementById("pais").value;
-    var representante = document.getElementById("representante").value;
-    var telefono = document.getElementById("tablaTelefono").value + ", ";
+  var columnatelefonoCelular = [];
 
-    var columnaTelefono = [];
-    
-    // Iterar sobre las filas de la tabla (a partir de la segunda fila)
-    for (var i = 1; i < tablaTelefono.rows.length; i++) {
-      var fila = tablaTelefono.rows[i];
-      var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
-      columnaTelefono.push(celdaTelefono);
-    }
-  
-    var telefono = columnaTelefono;
+  // Iterar sobre las filas de la tabla (a partir de la segunda fila)
+  for (var i = 1; i < tablaTelefonoCelular.rows.length; i++) {
+    var fila = tablaTelefonoCelular.rows[i];
+    var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
+    columnatelefonoCelular.push(celdaTelefono);
+  }
 
-    var correo = document.getElementById("correo").value;
-    var telefonoCelular = document.getElementById("tablaTelefonoCelular").value + ", ";
+  var telefonoCelular = columnatelefonoCelular;
 
-    var columnatelefonoCelular = [];
-    
-    // Iterar sobre las filas de la tabla (a partir de la segunda fila)
-    for (var i = 1; i < tablaTelefonoCelular.rows.length; i++) {
-      var fila = tablaTelefonoCelular.rows[i];
-      var celdaTelefono = fila.cells[0].innerText; // Obtener el valor de la segunda celda (columna de teléfono)
-      columnatelefonoCelular.push(celdaTelefono);
-    }
-   
-    var telefonoCelular = columnatelefonoCelular;
+  productora = new Productora(codigo, nombre, pais, representante, telefono, correo, telefonoCelular);
+  productoras.push(productora);
+  localStorage.setItem("productoras", JSON.stringify(productoras));
 
-    productora = new Productora(codigo, nombre, pais, representante, telefono, correo, telefonoCelular);
-    productoras.push(productora);
-    localStorage.setItem("productoras", JSON.stringify(productoras));
-
-    document.getElementById("codigo").focus();
+  document.getElementById("codigo").focus();
 
 }
 
 function consultarProductora() {
-    console.log("Productoras Registradas")
-    console.log(JSON.parse(localStorage.getItem("productoras") || "[]"))
+  console.log("Productoras Registradas")
+  console.log(JSON.parse(localStorage.getItem("productoras") || "[]"))
 }
